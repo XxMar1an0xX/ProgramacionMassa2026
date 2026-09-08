a662534c186182cb6ec75225f733f216f4e5d31c
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-09-08 15:36:28 -0300

    funciona con valores persistentes tras apagado

--- webtest/webtest.ino
+++ webtest/webtest.ino
@@ -1,19 +1,30 @@
 // #include <WiFi.h>
 #include <WebGUI.h>
 
-Toggle hola("LED", 50, 50);
+
+Slider angulo_servo("servo", 20, 20, 0, 270, 0, 350);
+Button salvar("guardar", 20, 50);
+
 void setup() {
   Serial.begin(115200);
-  GUI.addElement(&hola);
   GUI.startAP("ESP-webGUI", "hola");
-  GUI.setTitle("jkasdhkasj");
+  GUI.initSettings();
+  GUI.addElement(&angulo_servo);
+  GUI.addElement(&salvar);
+  GUI.setTitle("ServoControl");
   GUI.begin();
+
+  angulo_servo.setValue(GUI.loadIntSetting("Angulo"));
+
+  Serial.print("http://");
   Serial.println(GUI.getIP());
 }
 
 void loop() {
   GUI.update();
-  if (hola.isOn()) {
-    Serial.println("boton aprtado");
+
+  if (salvar.wasPressed()) {
+    GUI.saveSetting("Angulo", angulo_servo.getIntValue());
+    Serial.println("guardado");
   }
 }
