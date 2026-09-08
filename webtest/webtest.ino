fb1badca6d4940533b71ba80d543e59ea17d5feb
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-09-08 13:57:49 -0300

    webgui anda

--- webtest/webtest.ino
+++ webtest/webtest.ino
@@ -1,10 +1,19 @@
-#include <WiFi.h>
+// #include <WiFi.h>
 #include <WebGUI.h>
 
-
-
+Toggle hola("LED", 50, 50);
 void setup() {
+  Serial.begin(115200);
+  GUI.addElement(&hola);
+  GUI.startAP("ESP-webGUI", "hola");
+  GUI.setTitle("jkasdhkasj");
+  GUI.begin();
+  Serial.println(GUI.getIP());
 }
 
 void loop() {
+  GUI.update();
+  if (hola.isOn()) {
+    Serial.println("boton aprtado");
+  }
 }
