dda4394e960031a579db053d06da0a5d3e6aeb3e
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-09-16 13:06:14 -0300

    auth working

--- authtest/authtest.ino
+++ authtest/authtest.ino
@@ -1,5 +1,33 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "BOX5";
+const char* contraseña = "pintacasa(tuco)";
+
+AsyncWebServer gui_esp32(80);
 
 void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, contraseña);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("");
+  Serial.print("Conectado a Wifi, puerto: ");
+  Serial.println(WiFi.localIP());
+  gui_esp32.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    if (!request->authenticate("hola", "hola1")) {
+      return request->requestAuthentication();
+    }
+    request->send(
+      200,
+      "text/html",
+      "<h1>hola mundo</h1>");
+  });
+  gui_esp32.begin();
+  Serial.println("web server started");
 }
 
 void loop() {
