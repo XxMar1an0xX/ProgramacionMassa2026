dfc490f64919fe5275f54c73e70cb011c08817ac
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-09-17 13:51:34 -0300

    init ladron

---
+++ ladrondros/ladrondros.ino
@@ -0,0 +1,11 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+// const char* ssid = "BOX5";
+// const char* contraseña = "pintacasa(tuco)";
+void setup() {
+}
+
+void loop() {
+}
