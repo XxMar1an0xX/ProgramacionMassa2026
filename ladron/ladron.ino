db29b219dc0d4edee97e00f625cc4a16caa56161
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-09-18 13:18:13 -0300

    ajksdhasjkdas

--- ladron/ladron.ino
+++ ladron/ladron.ino
@@ -1,8 +1,10 @@
 #include <WebGUI.h>
 
-
 void setup() {
-}
-
-void loop() {
+  Serial.begin(115200);
+  GUI.startAP("ESPwebgui", "hola1234");
+  GUI.initSettings();
+  GUI.setTitle("control");
+  GUI.begin();
+  Serial.println(GUI.getIP());
 }
